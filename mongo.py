64422c31c9bd687bfdc3f7cf969b0cf9a0ddbc53
laura-stnz <laura.s.oliveira2018@gmail.com>
2026-07-13 19:13:41 -0300

    Colocando Cybell-J MongoDB na nuvem

--- mongo.py
+++ mongo.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
 
 # ==========================================
-# CONFIGURAÇÃO DO MONGODB (INTEGRANTE 2)
+# CONFIGURAÇÃO DO MONGODB NA NUVEM (ATLAS)
 # ==========================================
-# Conecta ao MongoDB local (certifique-se de ter o MongoDB instalado e rodando)
-mongo_client = MongoClient("mongodb://localhost:27017/")
+# Cole a sua URI do MongoDB Atlas aqui (já com a senha real no lugar de <password>):
+URI_NUVEM = "mongodb+srv://laurasoliveira2018_db_user:o2vxITy0n9PLY37Q@cybell-j.rtnzeaw.mongodb.net/?appName=Cybell-J"
+
+# Conecta ao MongoDB Atlas
+mongo_client = MongoClient(URI_NUVEM)
+
+# Mantém o mesmo nome do banco e das coleções
 mongo_db = mongo_client["cybell_db"]
 keystrokes_collection = mongo_db["keystrokes"]
 mouse_events_collection = mongo_db["mouse_events"]
